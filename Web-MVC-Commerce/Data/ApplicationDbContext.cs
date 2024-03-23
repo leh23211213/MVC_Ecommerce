9c1af7286666ebb2329665b954b840b262191717
Hiep Le <83544494+leh23211213@users.noreply.github.com>
2024-03-23 20:07:34 +0700

    update Domain_Map

--- Web-MVC-Commerce/Data/ApplicationDbContext.cs
+++ Web-MVC-Commerce/Data/ApplicationDbContext.cs
@@ -6,16 +6,31 @@ namespace Web_MVC_Commerce.Data
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
-
-        public DbSet<Users> Users { get; set; } = default!;
+            : base(options) { }
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<PaymentMethod> PaymentMethods { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<AccountType> AccountTypes { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
             modelBuilder.ApplyConfiguration(new UsersMap());
+            modelBuilder.ApplyConfiguration(new AdressMap());
+            modelBuilder.ApplyConfiguration(new PaymenMethodMap());
+            modelBuilder.ApplyConfiguration(new AccountMap());
+            modelBuilder.ApplyConfiguration(new AccountTypeMap());
+            modelBuilder.ApplyConfiguration(new CategoryMap());
+            modelBuilder.ApplyConfiguration(new ProductMap());
+            modelBuilder.ApplyConfiguration(new CartMap());
+            modelBuilder.ApplyConfiguration(new OrderMap());
+            modelBuilder.ApplyConfiguration(new OrderItemMap());
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
--- Web-MVC-Commerce/obj/Debug/net8.0/Web-MVC-Commerce.AssemblyInfo.cs
+++ Web-MVC-Commerce/obj/Debug/net8.0/Web-MVC-Commerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web-MVC-Commerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a78c68074d8158802f57a79ab920a216f117ac21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+851dbeacb252cbe0cdba021f0b0e9cf563977963")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web-MVC-Commerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web-MVC-Commerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
