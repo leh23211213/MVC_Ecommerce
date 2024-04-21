d50578abf8351651d331f36103c8dd7ee75ac8db
Hiep Le <83544494+leh23211213@users.noreply.github.com>
2024-04-21 20:20:36 +0700

    update layout

--- Web-MVC-Commerce/obj/Debug/net8.0/Web-MVC-Commerce.AssemblyInfo.cs
+++ Web-MVC-Commerce/obj/Debug/net8.0/Web-MVC-Commerce.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web-MVC-Commerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7946b266a6265595bdc5df2509684d04f69f5c57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+416c8b2f0137241c9c54f253fc945eacf825b6e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web-MVC-Commerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web-MVC-Commerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Web-MVC-Commerce/wwwroot/js/LoginPartial.js
+++ Web-MVC-Commerce/wwwroot/js/LoginPartial.js
@@ -1,10 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
     const profile = document.querySelector('.profile');
     const dropdown = document.querySelector('.dropdown__wrapper');
+    let isDropdownVisible = false; // Biến lưu trữ trạng thái của dropdown
 
     profile.addEventListener('click', () => {
-        dropdown.classList.remove('none');
+        isDropdownVisible = !isDropdownVisible; // Khi click vào profile, đảo ngược trạng thái của dropdown
+        if (isDropdownVisible) {
+            dropdown.classList.remove('none');
+        }
         dropdown.classList.toggle('hide');
+        dropdown.classList.toggle('dropdown__wrapper--fade-in');
     })
 
     document.addEventListener("click", (event) => {
@@ -12,10 +17,11 @@ document.addEventListener("DOMContentLoaded", function () {
         const isProfileClicked = profile.contains(event.target);
 
         if (!isClickInsideDropdown && !isProfileClicked) {
+            isDropdownVisible = false; // Nếu click bên ngoài dropdown và profile, đặt trạng thái của dropdown là false
             dropdown.classList.add('hide');
             dropdown.classList.remove('dropdown__wrapper--fade-in');
-        }
-        else {
+        } else {
+            isDropdownVisible = true; // Nếu click vào dropdown hoặc profile, đặt trạng thái của dropdown là true
             dropdown.classList.remove('hide');
             dropdown.classList.add('dropdown__wrapper--fade-in');
         }
